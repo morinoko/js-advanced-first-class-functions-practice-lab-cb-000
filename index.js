5526175479c9828fbc9f268dc5d3290619a19ccd
Felice Forby <feliceforby@gmail.com>
2019-02-05 22:36:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 function logDriverNames(drivers) {
-  drivers.forEach(function(element) {
-    console.log(element.name);
+  drivers.forEach(function(driverObject) {
+    console.log(driverObject.name);
   })
 }
